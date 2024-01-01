495677c3a537fe6f99fb95eff48635fe41807328
jon65 <xinquanyip@gmail.com>
2024-01-01 14:29:34 +1100

    push website

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'xinquanyip@gmail.com',
+    phone: '+61 4 9360 9987',
+    github: 'https://github.com/jon65',
+    linkedIn: 'https://www.linkedin.com/in/developerjy/',
+    medium: 'https://medium.com/@xinquanyip',
+}
---
+++ src/data/experienceData.js
@@ -0,0 +1,16 @@
+export const experienceData = [
+    {
+        id: 1,
+        company: 'Silkron Technologies',
+        jobtitle: 'Application Engineer Intern',
+        startYear: 'Sept 2022',
+        endYear: 'Dec 2022'
+    },
+    {
+        id: 2,
+        company: 'Mahkota Medical Centre',
+        jobtitle: 'Application Engineer Intern',
+        startYear: 'Jan 2022',
+        endYear: 'Feb 2022'
+    },
+]
